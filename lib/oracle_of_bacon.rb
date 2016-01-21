253d932007ae09bb791e59e55f7de61a41fc032d
grandnumber <thegrandnumber@gmail.com>
2016-01-21 11:18:52 -0800

    complete part 2

--- lib/oracle_of_bacon.rb
+++ lib/oracle_of_bacon.rb
@@ -43,6 +43,7 @@ class OracleOfBacon
       # your code here
     end
     # your code here: create the OracleOfBacon::Response object
+    @response = Response.new(xml)
   end
 
   def make_uri_from_arguments
@@ -63,14 +64,33 @@ class OracleOfBacon
     def parse_response
       if ! @doc.xpath('/error').empty?
         parse_error_response
-      # your code here: 'elsif' clauses to handle other responses
-      # for responses not matching the 3 basic types, the Response
-      # object should have type 'unknown' and data 'unknown response'
+      elsif ! @doc.xpath('/link').empty?
+        parse_graph_response
+      elsif ! @doc.xpath('/spellcheck').empty?
+        parse_spellcheck_response
+      else handle_unknown
       end
     end
     def parse_error_response
       @type = :error
       @data = 'Unauthorized access'
+    end
+
+    def parse_graph_response
+      @type = :graph
+      actors = @doc.xpath('//actor').map(&:text)
+      movies = @doc.xpath('//movie').map(&:text)
+      @data = actors.zip(movies).flatten.compact
+    end
+
+    def parse_spellcheck_response
+      @type = :spellcheck
+      @data = @doc.xpath('//match').map(&:text)
+    end
+
+    def handle_unknown
+      @type = :unknown
+      @data = 'unknown response type'
     end
   end
 end
